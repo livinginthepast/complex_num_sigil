06a12fdf915292f859e80089ad7d7bd67c711a02
Eric Saxby <sax@livinginthepast.org>
2017-12-16 12:15:29 -0800

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule ComplexNumSigil.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/livinginthepast/complex_num_sigil",
     ]
   end
 
@@ -20,7 +23,22 @@ defmodule ComplexNumSigil.Mixfile do
   defp deps do
     [
       {:complex_num, "~> 1.1"},
+      {:ex_doc, ">= 0.0.0", only: :dev},
       {:mix_test_watch, "~> 0.5", only: :dev, runtime: false},
+    ]
+  end
+
+  defp description() do
+    """
+    Adds a `~i` sigil for working with `ComplexNum`.
+    """
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      maintainers: ["Eric Saxby"],
+      links: %{"GitHub" => "https://github.com/livinginthepast/complex_num_sigil"},
     ]
   end
 end
